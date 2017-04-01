e6d87387b6dafc4bf45d9822b9c4a7fec7735db9
yan358941877 <yan358941877@163.com>
2017-04-01 10:08:39 +0800

    add module jquery

--- webpack-demo/app/index.js
+++ webpack-demo/app/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash';
+import $ from 'jquery';
 
 function component() {
-    var element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
+    //var element = document.createElement('div');
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    ////return element;
+
+    var element = $('<div></div>');
+    element.html(_.join(['Hello', 'webpack'], ' '));
+    return element.get(0);
 }
 
 document.body.appendChild(component());
