2997833fdf78054c1d5d4e90c8a7969bb63d6329
yan358941877 <yan358941877@163.com>
2017-04-10 20:09:49 +0800

    类组件

--- hello-react/src/Clock.js
+++ hello-react/src/Clock.js
@@ -20,6 +20,23 @@ class Clock extends React.Component{
         super(props);
         this.state = {date: new Date()};
     }
+
+    componentDidMount(){
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
     render () {
         return (
         <div>
@@ -29,3 +46,4 @@ class Clock extends React.Component{
         );
     }  
 }
+export default Clock;
--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -2,28 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Welcome from './Welcome';
+import Clock from './Clock';
 import './index.css';
 
 ReactDOM.render(
     <Welcome name="闫鑫"/>,
     document.getElementById('root')
 );
-ReactDOM.render( 
-    <h1> Hello, React! </h1>,
-    document.getElementById('root02')
-);
+// ReactDOM.render( 
+//     <h1> Hello, React! </h1>,
+//     document.getElementById('root02')
+// );
 
 //不够"模块化",无法实现复用
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, World!</h1>
-            <h1>It is {new Date().toLocaleString()}.</h1>
-        </div>
-    );
-    ReactDOM.render(
-        element,
-        document.getElementById('root02')
-    );
-}
-setInterval(tick, 1000);
+// function tick() {
+//     const element = (
+//         <div>
+//             <h1>Hello, World!</h1>
+//             <h1>It is {new Date().toLocaleString()}.</h1>
+//         </div>
+//     );
+//     ReactDOM.render(
+//         element,
+//         document.getElementById('root02')
+//     );
+// }
+// setInterval(tick, 1000);
+
+ReactDOM.render(
+    <Clock />,
+    document.getElementById("root02")
+)
