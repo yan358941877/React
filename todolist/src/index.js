79852eb0b29347b17ec6bac74770afccbeffdcd8
yan358941877 <yan358941877@163.com>
2017-04-12 22:14:43 +0800

    添加 normalize.css

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoApp from './TodoApp';
 import './index.css';
+import 'normalize.css';
+import 'reset.css';
 
 ReactDOM.render(
   <TodoApp />,
