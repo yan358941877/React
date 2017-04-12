6a6ebc6e0ead07d7ea29f4cd9d473f2362afbe94
yan358941877 <yan358941877@163.com>
2017-04-12 21:34:00 +0800

    添加TodoItem

--- todolist/src/TodoApp.js
+++ todolist/src/TodoApp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TodoInput from "./TodoInput";
+import TodoItem from "./TodoItem";
 
 class TodoApp extends React.Component {
     constructor(props) {
@@ -11,7 +12,8 @@ class TodoApp extends React.Component {
                 <h2>待办事项</h2>
                 <TodoInput />
                 <ul>
-                    {}
+                    <TodoItem key="1"/>
+                    <TodoItem key="2"/>
                 </ul>
             </div>
         )
---
+++ todolist/src/TodoItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class TodoItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <li>待办事项</li>
+        )
+    }
+}
+
+export default TodoItem;
