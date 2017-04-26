05e272f244848f416e213d420f9e56a4ab2583ac
yan358941877 <yan358941877@163.com>
2017-04-26 20:28:01 +0800

    输入功能的实现

--- comment-app/src/CommentInput.js
+++ comment-app/src/CommentInput.js
@@ -1,9 +1,60 @@
 import React, {Component} from 'react'
 
 class CommentInput extends Component{
+    constructor(){
+        super()
+        this.state = {
+            username: '',
+            content: ''
+        }
+    }
+   
+    handleUsernameChange(event){
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handleContentChange(event){
+        this.setState({
+            content: event.target.value
+        })
+    }
+    handleClick(event){
+        // 先判断在props中是否存在由父级组件传来的回掉函数
+        if(this.props.onSubmit){
+            const {username, content} = this.state
+            this.props.onSubmit({username, content})
+        }
+        this.setState({
+            content: ''
+        })
+    }
     render(){
         return (
-            <div>CommentInput</div>
+            <div className='comment-input'>
+                <div className="comment-field">
+                    <span className="comment-field-name">用户名：</span>
+                    <div className="comment-field-input">
+                        <input 
+                            type="text" 
+                            value={this.state.username}
+                            onChange={this.handleUsernameChange.bind(this)}/>
+                    </div>
+                </div>
+                <div className="comment-field">
+                    <span className="comment-field-name">评论内容：</span>
+                    <div className="comment-field-input">
+                        <textarea 
+                            value={this.state.contentd}
+                            onChange={this.handleContentChange.bind(this)}/>
+                    </div>
+                </div>
+                <div className="comment-field-button">
+                    <button onClick={this.handleSubmit.bind(this)}>
+                        发布
+                    </button>
+                </div>
+            </div>
         )
     }
 }
