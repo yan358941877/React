a299611025a2127425d8660d4863c5ce1a4ee618
yan358941877 <yan358941877@163.com>
2017-04-26 19:35:10 +0800

    基本框架的搭建

--- comment-app/src/index.js
+++ comment-app/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CommentApp from './CommentApp';
 import './index.css';
 
+
 ReactDOM.render(
-  <App />,
+  <CommentApp />,
   document.getElementById('root')
 );
