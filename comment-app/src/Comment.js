a04fb655ddd8073299568f2446086586d979efac
yan358941877 <yan358941877@163.com>
2017-04-27 10:16:00 +0800

    显示代码块

--- comment-app/src/Comment.js
+++ comment-app/src/Comment.js
@@ -1,45 +1,58 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 class Comment extends Component {
-    constructor(){
+    constructor() {
         super()
-        this.state = {timeString: ''}
+        this.state = { timeString: '' }
     }
-    _updateTimeString(){
+    _updateTimeString() {
         const comment = this.props.comment
-        const duration = (+Date.now() - comment.createTime)/1000
+        const duration = (+Date.now() - comment.createTime) / 1000
         this.setState({
             timeString: duration > 60
-            ? `${Math.round(duration/60)} 分钟前`
-            : `${Math.round(Math.max(duration, 1))} 秒前`
+                ? `${Math.round(duration / 60)} 分钟前`
+                : `${Math.round(Math.max(duration, 1))} 秒前`
         })
     }
-    componentWillMount(){
+    componentWillMount() {
         this._updateTimeString()
         this._timer = setInterval(
             this._updateTimeString.bind(this),
             5000
         )
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
         clearInterval(this._timer)
     }
-    handleDeleteComment(){
-        if(this.props.onDeleteComment){
+    handleDeleteComment() {
+        if (this.props.onDeleteComment) {
             this.props.onDeleteComment(this.props.index)
         }
     }
-    render(){
+    _getProcessedContent(content) {
+        return content
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;")
+            .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+    }
+    render() {
         return (
             <div className='comment'>
                 <div className='comment-user'>
                     <span>{this.props.comment.username}</span>
                 </div>
-                <p>{this.props.comment.content}</p>
+                <p
+                    dangerouslySetInnerHTML={{
+                        __html: this._getProcessedContent(this.props.comment.content)
+                    }}
+                />
                 <span className='comment-createtime'>
                     {this.state.timeString}
                 </span>
-                <span 
+                <span
                     className='comment-delete'
                     onClick={this.handleDeleteComment.bind(this)}>
                     删除
