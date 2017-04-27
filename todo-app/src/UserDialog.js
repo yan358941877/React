6b47f782489b022b003b60d985bff9cc88d95277
yan358941877 <yan358941877@163.com>
2017-04-27 19:44:24 +0800

    登录注册对话框的实现

--- todo-app/src/UserDialog.js
+++ todo-app/src/UserDialog.js
@@ -1,26 +1,80 @@
 import React, {Component} from 'react'
 import './UserDialog.css'
 class UserDialog extends Component {
+    constructor(){
+        super()
+        this.state = {
+            username: '',
+            password: '',
+            login: true
+        }
+    }
+    handleChangeOperate(event){
+        if(event.target.value ==='login'){
+            this.setState({
+                login: true
+            })
+        }else if(event.target.value === 'signup'){
+            this.setState({
+                login: false
+            })
+        }
+    }
+    handleChangeUsername(event){
+        this.setState({
+            username: event.target.value
+        })
+    }
+    handleChangePass(event){
+        this.setState({
+            password: event.target.value
+        })
+    }
+    handleSubmit(event){
+        if(!this.state.username){
+            return alert("请输入用户名")
+        }
+        if(!this.state.password){
+            return alert("请输入密码")
+        }
+        if(this.state.login){
+            // alert(this.state.username)
+            this.props.onSubmit(this.state)
+        }else {
+            //alert(this.state.password)
+            this.props.onSubmit(this.state)
+        }
+    }
     render(){
         return (
             <div className='UserDialog'>
                 <div className="dialog-panel">
-                    <div className='user-operate'>
-                        <input type="radio" name="login" id="login"/>
+                    <div className='user-operate' onChange={this.handleChangeOperate.bind(this)}>
+                        <input 
+                            type="radio" 
+                            name="operate" 
+                            id="login" 
+                            checked={this.state.login}
+                            value="login"/>
                         <label htmlFor="login">登录</label>
-                        <input type="radio" name="login" id="signup"/>
+                        <input 
+                            type="radio" 
+                            name="operate" 
+                            id="signup"
+                            checked={!this.state.login}
+                            value="signup"/>
                         <label htmlFor="signup">注册</label>
                     </div>
                     <div className="user-info">
                         <span>用户名</span>
-                        <input type="text" />
+                        <input type="text" onChange={this.handleChangeUsername.bind(this)}/>
                     </div>
                     <div className="user-info">
                         <span>密码</span>
-                        <input type="password" />
+                        <input type="password" onChange={this.handleChangePass.bind(this)}/>
                     </div>
                     <div className="user-submit">
-                        <button>登录</button>
+                        <button onClick={this.handleSubmit.bind(this)}>{this.state.login?'登录':'注册'}</button>
                     </div>
                 </div>
             </div>    
