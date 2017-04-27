5be03a2a301038ef2177535992ca1234698f2903
yan358941877 <yan358941877@163.com>
2017-04-27 08:48:00 +0800

    用户名持久化

--- comment-app/src/CommentInput.js
+++ comment-app/src/CommentInput.js
@@ -1,59 +1,79 @@
-import React, {Component} from 'react'
+import React, { Component, PropTypes } from 'react'
 
-class CommentInput extends Component{
+class CommentInput extends Component {
     // 组件参数验证，指定props中的onSubmit的类型为函数类型
     static propTypes = {
         onSubmit: PropTypes.func
     }
-    constructor(){
+    constructor() {
         super()
         this.state = {
             username: '',
             content: ''
         }
     }
-    componentDidMount(){
+    componentWillMount(){
+        this._localUsername()
+    }
+    _localUsername(){
+        const username = localStorage.getItem('username')
+        if(username){
+            this.setState({
+                username:username
+            })
+        }
+    }
+    componentDidMount() {
         this.textarea.focus()
     }
-    handleUsernameChange(event){
+    /* input相关的事件处理函数 */
+    handleUsernameChange(event) {
         this.setState({
             username: event.target.value
         })
     }
-    handleContentChange(event){
+    // _saveUsername作为私有方法所以以_开头
+    _saveUsername(username) {
+        localStorage.setItem('username', username)
+    }
+    handleUsernameBlur(event) {
+        this._saveUsername(event.target.value);
+    }
+    handleContentChange(event) {
         this.setState({
             content: event.target.value
         })
     }
-    handleSubmit(event){
+    handleSubmit(event) {
         // 先判断在props中是否存在由父级组件传来的回掉函数
-        if(this.props.onSubmit){
-            const {username, content} = this.state
-            this.props.onSubmit({username, content})
+        if (this.props.onSubmit) {
+            const { username, content } = this.state
+            this.props.onSubmit({ username, content })
         }
         this.setState({
             content: ''
         })
     }
-    render(){
+    render() {
         return (
             <div className='comment-input'>
                 <div className="comment-field">
                     <span className="comment-field-name">用户名：</span>
                     <div className="comment-field-input">
-                        <input 
-                            type="text" 
+                        <input
+                            type="text"
                             value={this.state.username}
-                            onChange={this.handleUsernameChange.bind(this)}/>
+                            onChange={this.handleUsernameChange.bind(this)}
+                            onBlur={this.handleUsernameBlur.bind(this)} />
                     </div>
                 </div>
                 <div className="comment-field">
                     <span className="comment-field-name">评论内容：</span>
                     <div className="comment-field-input">
-                        <textarea 
-                            ref={(textarea)=>this.textarea=textarea}
-                            onChange={this.handleContentChange.bind(this)} 
-                            value={this.state.content}/>  
+                        <textarea
+                            ref={(textarea) => this.textarea = textarea}
+                            onChange={this.handleContentChange.bind(this)}
+                            value={this.state.content} />
                     </div>
                 </div>
                 <div className="comment-field-button">
